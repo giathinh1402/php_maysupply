9fb304489b1bf8e73440510fecb2e4c822445764
Thinh Gia Nguyen <thinh-gia.nguyen@corp.seadev.com.vn>
2020-06-29 18:37:52 +0700

    php_maysupply

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <h2>header</h2>
+        <?php include 'menu.php';?>
+    </header>
+</body>
+
+</html>
